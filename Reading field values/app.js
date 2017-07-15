54e27b318e35e21aa9268af4f76a8505a80a480c
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2017-07-15 17:32:32 +0500

    few errors are removed from reading field values

--- Reading field values/app.js
+++ Reading field values/app.js
@@ -6,11 +6,7 @@
 
 function checkAddress(fieldId) {
     var val = document.getElementById(fieldId).value;
-    var pas = document.getElementById('pass').value;
     if (val === "") {
         alert("Email address required.");
-    }
-    if (pas < 8 ) {
-        alert('password is short');
     }
 } 
