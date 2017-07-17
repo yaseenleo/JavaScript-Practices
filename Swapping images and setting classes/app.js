714525e517d4b5e9c41076e0649f6c972ccd5a27
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2017-07-17 15:41:37 +0500

    lesson swapping images and setting classes is complete now moving forward to setting style

---
+++ Swapping images and setting classes/app.js
@@ -0,0 +1,18 @@
+function swap() {
+    document.getElementById("kurama").src = "img/sasuke.jpg"
+}
+
+function swapPic() {
+    var pic = document.getElementById("sasuke")
+    pic.src = "img/naruto.jpg"
+}
+
+function getAddress() {
+    var link = document.getElementById("link1")
+    link.href = "https://www.facebook.com"
+}
+
+function enlargeText() {
+    var text = document.getElementById("text")
+    text.className = "doubleSize"
+}
