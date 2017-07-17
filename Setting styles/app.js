036921ebfdc0a104b3f1501748799de39bfb2ff3
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2017-07-18 00:45:40 +0500

    lesson setting style is complete now heading toward target all elements by tag name

---
+++ Setting styles/app.js
@@ -0,0 +1,12 @@
+function floatLeft() {
+    // this statement mright-floats an image
+    document.getElementById("pic").style.cssFloat = "right";
+}
+function imgHidden() {
+    // this statement makes an image invisible
+    document.getElementById("hide").style.visibility = "hidden";
+}
+function imgMargin() {
+    // this statement gives an element left and right margin of 100px
+    document.getElementById("mainPic").style.margin = "0 100px 0 100px";
+}
