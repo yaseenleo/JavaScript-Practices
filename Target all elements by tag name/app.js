869f44261a587f3114f052d0cf69177710d9ea69
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2017-07-18 13:12:55 +0500

    lesson target all elements by tag name is complete. now heading forward to target some elements by tag name

---
+++ Target all elements by tag name/app.js
@@ -0,0 +1,15 @@
+var par = [];
+par = document.getElementsByTagName("p");
+function changeText() {
+    par[2].innerHTML = "This SUV is too big.";
+}
+console.log(par)
+// for (var i = 0; i < par.length; i++) {
+//     par[i].style.fontFamily = "Verdana, Geneva, sans-serif";
+// }
+
+// // this statement makes a collection of all images in the documents and assigns it to the variable pics
+// var pics = document.getElementsByTagName("img");
+
+// // This statement makes a collection of all the unordered lists in the document and assigns it to the variable ulists. 
+// var ulists = document.getElementsByTagName("ul"); 
