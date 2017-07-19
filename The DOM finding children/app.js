a371ac474002f506f6de1bd4ed896043235eb8cd
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2017-07-19 13:00:18 +0500

    DOM finding children and junk artifacts and nodeType is complete now moving toward DOM more ways to target elements

---
+++ The DOM finding children/app.js
@@ -0,0 +1,8 @@
+var d = document.getElementsByTagName("p");
+// var contents = d[2].innerHTML;
+// console.log(contents);
+
+var div = document.getElementById("ny");
+var p = div.getElementsByTagName("p");
+var content = p[1].innerHTML
+console.log(content);
