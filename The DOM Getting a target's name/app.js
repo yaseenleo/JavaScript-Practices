873b33b09c33329b7d0ba42aa04a7baed0079a5e
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2017-07-19 18:26:18 +0500

    DOM Getting target name is complete now starting exercise DOM Counting elements

---
+++ The DOM Getting a target's name/app.js
@@ -0,0 +1,6 @@
+var parent = document.getElementById("div1");
+var target = parent.firstChild;
+var nTextContent = target.nodeName;
+var nTextContentValue = target.nodeValue;
+console.log(nTextContent);
+console.log(nTextContentValue);
