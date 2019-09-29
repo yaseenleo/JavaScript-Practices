f3089bf3fcf22bf669e48382550bf01131472724
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2019-09-29 15:10:38 +0500

    boolean practice leasson

---
+++ ES6 JavaScript Practice/01Basics/nullandBool.js
@@ -0,0 +1,20 @@
+var temp = 0;
+
+console.log("Current temperature is "+ temp)
+
+
+// Boolean Operation - Boolean operation is a operation which returns you true or false in return
+
+let actualMArks = 7;
+
+let myGrade =  (actualMArks < 10)
+
+console.log(myGrade)
+
+let actualMArks1 = 10;
+
+let myGrade1 =  (actualMArks1 == 10)
+
+console.log(myGrade1)
+
+// in programming we use one equal to asign right hand side value to left hand side but when you use two equal sign we use it for comparison
--- ES6 JavaScript Practice/01Basics/readMe.txt
+++ ES6 JavaScript Practice/01Basics/readMe.txt
@@ -4,3 +4,7 @@ write code node HelloWorld.js (file name exactly you have in your directory)
 in let the value in variable can be change but in const if a value is already declared it can not change its a rigid value
 
 redeclaring of same variable is not allowed
+
+Boolean Operation - Boolean operation is a operation which returns you true or false in return
+
+in programming we use one equal to asign right hand side value to left hand side but when you use two equal sign we use it for comparison
