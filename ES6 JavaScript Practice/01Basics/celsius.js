d092fb49dc38f71922160a74411373b6cd82c3ac
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2019-09-29 12:24:58 +0500

    number manipulation lesson complete by converting fahrenheit to celsuis

---
+++ ES6 JavaScript Practice/01Basics/celsius.js
@@ -0,0 +1,21 @@
+var score = 100
+
+score = 130;
+
+const bonus = 50;
+
+var finalScore = (score + bonus) * 1.8;
+
+var complex = ((4 + 4) * 5) / 2  //using parenthesis in complex number equation is a good practice
+
+console.log(finalScore)
+console.log(complex)
+
+let temInFahrenheit = 100;
+
+// do calculation
+    // (T(°F) - 32) × 5/9
+
+    let celsius = (temInFahrenheit - 32) * 5/9
+
+    console.log(celsius)
