737693227fefa380cb0cfd0f2b462bb025f6a699
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2019-11-04 19:11:49 +0500

    forEach function loop exercise done

---
+++ ES6 JavaScript Practice/01Intermediate/forEachLoop.js
@@ -0,0 +1,27 @@
+const days = ['Mon','Tue','Wed','Thr','Fri','Sat'];
+
+console.log(days[0])
+
+days.forEach(function(day, index){
+    console.log(`starts with ${index+1} -- ${day}`)
+});
+
+//in forEach function the first parameter should be element it self in the array and second should be the index
+
+const months = ['January', 'Fabuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+months.forEach( (month, index)=>{
+    if(index === 0){
+        console.log(`The ${index+1}st month is ${month}`)
+    }
+    else if(index+1 === 2) {
+        console.log(`The ${index+1}nd month is ${month}`)
+    }
+    else if(index+1 === 3) {
+        console.log(`The ${index+1}rd month is ${month}`)
+    }
+    else{
+        console.log(`The ${index+1}th month is ${month}`)
+
+    }
+})
---
+++ ES6 JavaScript Practice/01Intermediate/readMe.txt
@@ -0,0 +1,6 @@
+To create a function
+1. declaration and name the function
+2. the function itself block of code
+3. call of function
+
+in forEach function the first parameter should be element it self in the array and second should be the index
