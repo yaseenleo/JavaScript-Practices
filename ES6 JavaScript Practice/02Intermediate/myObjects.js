35443ec2731913e941e9df344cfa94d55f7d61c9
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2019-11-15 16:48:31 +0500

    object practice exercise

---
+++ ES6 JavaScript Practice/02Intermediate/myObjects.js
@@ -0,0 +1,23 @@
+let myYoutubeVideo = {
+    title: "Loops in JavaScript",
+    videoLength: 15,
+    videoDescription: "this is a video description and a long one",
+    videoCreater: "M. Yaseen"
+}
+
+console.log(myYoutubeVideo);
+
+console.log(`Hey new video on ${myYoutubeVideo.title} by ${myYoutubeVideo.videoCreater}`);
+
+// assignment javascript course name price description
+
+let myCourse = {
+    courseName: "JavaScript",
+    price: 20,
+    description: "this course is for beginners where you can understand the fundamentals of it"
+}
+
+console.log(myCourse);
+console.log(`This Course name is ${myCourse.courseName} and its price is ${myCourse.price}`);
+console("Description: " + myCourse.description)
+
--- ES6 JavaScript Practice/02Intermediate/readMe.txt
+++ ES6 JavaScript Practice/02Intermediate/readMe.txt
@@ -2,3 +2,6 @@ when we use == it just checks equality
 when we use === it checks it is exactly equal to both side     
 == operator (Equality)
 === operator (Identity)
+
+in object the object values can be string, number, boolean, data.
+in object we use colon rather then equal to and to end the key and value we use coma
