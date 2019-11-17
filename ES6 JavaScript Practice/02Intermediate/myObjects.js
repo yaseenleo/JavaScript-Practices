af2df36c52f1ea075e9f3186e9a30b2f4d2bdaa9
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2019-11-17 21:00:12 +0500

    function that takes objects and gives object

--- ES6 JavaScript Practice/02Intermediate/myObjects.js
+++ ES6 JavaScript Practice/02Intermediate/myObjects.js
@@ -1,15 +1,20 @@
-let myYoutubeVideo = {
+let myYoutubeVideoOne = {
     title: "Loops in JavaScript",
     videoLength: 15,
     videoDescription: "this is a video description and a long one",
     videoCreater: "M. Yaseen"
 }
 
-console.log(myYoutubeVideo);
+let myYoutubeVideoTwo = {
+    title: "Loops in JavaScript",
+    videoLength: 15,
+    videoDescription: "this is a video description and a long one",
+    videoCreater: "M. Yaseen"
+}
 
-console.log(`Hey new video on ${myYoutubeVideo.title} by ${myYoutubeVideo.videoCreater}`);
+console.log(myYoutubeVideoOne);
 
-// assignment javascript course name price description
+console.log(`Hey new video on ${myYoutubeVideoOne.title} by ${myYoutubeVideoOne.videoCreater}`);
 
 let myCourse = {
     courseName: "JavaScript",
@@ -19,5 +24,16 @@ let myCourse = {
 
 console.log(myCourse);
 console.log(`This Course name is ${myCourse.courseName} and its price is ${myCourse.price}`);
-console("Description: " + myCourse.description)
+console.log("Description: " + myCourse.description)
 
+
+let changeVideoLength = function(myObject) {
+    return{
+        formatOne: `Time of this video is: ${myObject.videoLength + 2}`,
+        formatTwo: `Time of this video is: ${myObject.videoLength + 3}`
+    };
+    
+}
+
+let adOne = changeVideoLength(myYoutubeVideoOne);
+console.log(adOne.formatOne);
