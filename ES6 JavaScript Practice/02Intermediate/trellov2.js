8cd62bb44323efa9a63c5b234a08291d4241e334
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2019-11-18 00:38:47 +0500

    trello example of meeting add and done

---
+++ ES6 JavaScript Practice/02Intermediate/trellov2.js
@@ -0,0 +1,30 @@
+let myTodos = {
+    day: 'Monday',
+    meetings: 0,
+    meetDone: 0,
+}
+
+let addMeeting = function(todo, meet = 0) {
+    todo.meetings = todo.meetings + meet
+}
+
+let meetDone = function(todo, meet = 0){
+    todo.meetDone = todo.meetDone + meet
+}
+
+let getSummaryOfDay = function(todo){
+    let meetLeft = todo.meetings - todo.meetDone
+    return `You have ${meetLeft} meetings today!`
+}
+
+let reset = function(todo){
+    todo.meetings = 0;
+    todo.meetDone = 0;
+}
+
+addMeeting(myTodos, 4)
+addMeeting(myTodos, 2)
+meetDone(myTodos, 5)
+getSummaryOfDay(myTodos);
+
+console.log(getSummaryOfDay(myTodos))
