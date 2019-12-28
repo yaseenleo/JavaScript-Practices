5ce0d5ba11b0e3bf3bde712fc8e3ebad75fe1b8c
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2019-12-28 15:34:13 +0500

    event listeners

--- ES6 JavaScript Practice/01WebBasics/myscript.js
+++ ES6 JavaScript Practice/01WebBasics/myscript.js
@@ -36,3 +36,9 @@ const myNewPara = document.createElement('p');
 myNewPara.textContent = 'I was added via JS';
 
 document.querySelector('body').appendChild(myNewPara);
+
+document.querySelector('button').addEventListener('click', (event)=>{
+    console.log('Button was pressed', event);
+    event.target.textContent = 'I was clicked';
+    
+})
