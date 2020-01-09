9b83c397644be529622485d9133fc67a36de2d35
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2020-01-10 02:14:14 +0500

    basics of callback, Promises, Async Function and await

---
+++ ES6 JavaScript Practice/Async JS/promises.js
@@ -0,0 +1,70 @@
+const posts = [
+    {title: 'Post One', body: 'This is post one'},
+    {title: 'Post Two', body: 'This is post two'},
+]
+
+function getPosts(){
+    setTimeout(() =>{
+        let output = '';
+        posts.forEach((post, index) =>{
+            output += `<li>${post.title}</li>`
+        })
+
+        document.body.innerHTML = output
+    },1000)
+}
+
+function createPost(post) {
+    return new Promise((resolve, reject) =>{
+ 
+        setTimeout(() =>{
+            posts.push(post);
+            
+            const error = false;
+
+            if (!error) {
+                resolve()
+            }
+            else{
+                reject('Error: Something is wrong')
+            }
+        },2000)
+    });
+}
+
+    //this method is used when you have a single response but if you have more then one promises resolves we use promise.all
+// createPost({title: 'Post Three',body:'This is post three'})
+// .then(getPosts)
+// .catch(err => console.log(err)
+// )
+
+// async / await
+async function init(){
+   await createPost({title: 'Post Three',body:'This is post three'})
+
+   getPosts();
+}
+init()
+
+// async / await / fetch
+async function fetchUsers() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+
+    console.log(data);
+    
+}
+
+fetchUsers()
+
+//Promise.all
+// const promise1 = Promise.resolve('Hello World');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve,reject)=>{
+//     setTimeout(resolve, 2000, 'GoodBye')
+// });
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+
+// Promise.all([promise1,promise2, promise3, promise4]).then( values =>
+//     console.log(values)
+//     );
